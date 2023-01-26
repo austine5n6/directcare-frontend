f81a6d58b5f68f46fe5ed21773530a66108004c0
jude4 <judeufuomao@gmail.com>
2023-01-26 18:20:36 +0100

    fix errors

--- models/index.js
+++ models/index.js
@@ -40,4 +40,23 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+
+db.user = require("../models/user.js")(sequelize, Sequelize);
+db.role = require("../models/role.js")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId"
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
+
 module.exports = db;
